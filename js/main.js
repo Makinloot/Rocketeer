8cc45fa3ac7b84133892472465d94751bae1b26d
Tornike Epitashvili <itorn9777@gmail.com>
2022-03-25 10:39:01 -0700

    updated & fixed change page and input validations

--- js/main.js
+++ js/main.js
@@ -1,55 +1,58 @@
-const section1 = document.getElementById('section-1');
-const section2 = document.getElementById('section-2');
-const section3 = document.getElementById('section-3');
+const section1 = document.getElementById("section-1");
+const section2 = document.getElementById("section-2");
+const section3 = document.getElementById("section-3");
+const section4 = document.getElementById("section-4");
+const section5 = document.getElementById("section-5");
 
-const firstName = document.getElementById('first-name');
-const lastName = document.getElementById('last-name');
-const email = document.getElementById('email');
-const telephone = document.getElementById('telephone');
-const firstNameError = document.getElementById('first-name-error');
-const lastNameError = document.getElementById('last-name-error');
-const emailError = document.getElementById('email-error');
-const telephoneError = document.getElementById('telephone-error');
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const telephone = document.getElementById("telephone");
+const firstNameError = document.getElementById("first-name-error");
+const lastNameError = document.getElementById("last-name-error");
+const emailError = document.getElementById("email-error");
+const telephoneError = document.getElementById("telephone-error");
 
-// checks inputs and if valid allows to change page
-// otherwise returns input errors.
-const changePage = (currentPage, nextPage) => {
-    const isValidName = document.getElementById('first-name').value;
-    const isValidLastName = document.getElementById('last-name').value;
-    const isValidemail = document.getElementById('email').value;
+const SECTION2_NEXT_BTN = document.getElementById("section2__next-btn");
 
-    if(!isValidName || !isValidLastName || !isValidemail) {
-        validateInputs(firstName, firstNameError);
-        validateInputs(lastName, lastNameError);
-        validateInputs(email, emailError);
-    } else {
-        currentPage.style.display = 'none';
-        nextPage.style.display = 'grid';
-    }
-}
-
-// allows to return on previous page without
-// validating input forms.
-const previousPage = (currentPage, prevPage) => {
-    currentPage.style.display = 'none';
-    prevPage.style.display = 'grid';
-}
+// displays current page as 'none' and
+// next page as 'grid', but displays
+// starting page as 'flex'
+const changePage = (currentPage, prevPage) => {
+  currentPage.style.display = "none";
+  if (prevPage == section1) {
+    prevPage.style.display = "flex";
+  } else {
+    prevPage.style.display = "grid";
+  }
+};
 
 // checks and validates input forms
 const validateInputs = (input, inputError) => {
-    if(!input.validity.valid) {
-        inputError.innerText = 'Fill form correctly';
+  if (!input.validity.valid) {
+    inputError.innerText = "Fill form correctly";
+    if (input.validity.typeMismatch) {
+      inputError.innerText = "Enter correct E-Mail";
+    } else if (input.validity.tooShort) {
+      inputError.innerText = "Form too short";
+    } else {
+      inputError.innerText = "Required field";
     }
-    if(input.validity.valueMissing) {
-        inputError.innerText = 'Required  field';
-    }
-    if(input.validity.tooShort) {
-        inputError.innerText = 'Form value too short';
-    }
-    if(input.validity.typeMismatch) {
-        inputError.innerText = 'Enter correct E-Mail';
-    }
-    if(input.validity.valid) {
-        inputError.innerText = '';
-    }
-}
+    return false;
+  } else {
+    inputError.innerText = "";
+    return true;
+  }
+};
+
+// allows to move on next page,
+// after checking input validations.
+const allowNextPage = () => {
+  const validName = validateInputs(firstName, firstNameError);
+  const validSurname = validateInputs(lastName, lastNameError);
+  const validEmail = validateInputs(email, emailError);
+
+  if (validName && validSurname && validEmail) {
+    SECTION2_NEXT_BTN.addEventListener("click", changePage(section2, section3));
+  }
+};
